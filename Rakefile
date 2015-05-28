b90544d51000ad80da9779513dd098a3c5128ad5
Swoodend <woodendscott@gmail.com>
2015-05-28 17:43:25 +0000

    implement db:populate in rakefile

--- Rakefile
+++ Rakefile
@@ -1,6 +1,8 @@
 require 'rake'
 require "sinatra/activerecord/rake"
+require 'active_record'
 require ::File.expand_path('../config/environment', __FILE__)
+require "./lib/users_importer"
 
 Rake::Task["db:create"].clear
 Rake::Task["db:drop"].clear
@@ -19,4 +21,9 @@ end
 desc 'Retrieves the current schema version number'
 task "db:version" do
   puts "Current version: #{ActiveRecord::Migrator.current_version}"
+end
+
+desc 'populate the database with seed data'
+task 'db:populate' do
+  UsersImporter.new.import
 end
---
+++ lib/users_importer.rb
@@ -0,0 +1,53 @@
+class UsersImporter
+
+  def initialize(filename=File.dirname(__FILE__) + "/db/seed_data.sql")
+    @filename = filename
+  end
+
+  def import
+    puts "importing students from #{@filename}"
+
+    database = ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: 'db/db.sqlite3'
+    )
+    
+    database.connection.execute("INSERT INTO USERS(id, username, email, password, created_at, updated_at)
+      VALUES(1, 'John', 'john@gmail.com', 'password1234', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO USERS(id, username, email, password, created_at, updated_at)
+      VALUES(2, 'Stacey', 'stacey@gmail.com', 'p1234321', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO USERS(id, username, email, password,created_at, updated_at)
+      VALUES(3, 'Kevin', 'kev@gmail.com', 'kev28883', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO SKILLS(id, name, created_at, updated_at)
+      VALUES(1, 'programming', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);") 
+
+    database.connection.execute("INSERT INTO SKILLS(id, name, created_at, updated_at)
+      VALUES(2, 'plumbing', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO SKILLS(id, name, created_at, updated_at)
+      VALUES(3, 'yoga', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);") 
+
+    database.connection.execute("INSERT INTO SKILLS(id, name, created_at, updated_at)
+      VALUES(4, 'surfing', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO RELATIONSHIPS(id, users_id, skills_id, type, created_at, updated_at) 
+      VALUES(1, 1, 1, 'learnable', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO RELATIONSHIPS(id, users_id, skills_id, type, created_at, updated_at) 
+      VALUES(2, 1, 2, 'teachable', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO RELATIONSHIPS(id, users_id, skills_id, type, created_at, updated_at)  
+      VALUES(3, 2, 2, 'learnable', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO RELATIONSHIPS(id, users_id, skills_id, type, created_at, updated_at) 
+      VALUES(4, 2, 1, 'teachable', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+    database.connection.execute("INSERT INTO RELATIONSHIPS(id, users_id, skills_id, type, created_at, updated_at) 
+      VALUES(5, 3, 2, 'learnable', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);")
+
+  end
+
+end
